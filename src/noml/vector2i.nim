a1b6e4112f92e1a80883034fa6846dcdb57cbd79
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-08-09 19:51:50 -0400

    Nice

--- src/noml/vector2i.nim
+++ src/noml/vector2i.nim
@@ -3,19 +3,19 @@ type
     x: int = 0
     y: int = 0
 
-proc getX*(v: Vector3i): int =
+proc getX*(v: Vector2i): int =
   return v.x
 
-proc getY*(v: Vector3i): int =
+proc getY*(v: Vector2i): int =
   return v.y
 
-proc setX*(v: Vector3i, x: int): Vector3i {.discardable.} =
+proc setX*(v: Vector2i, x: int): Vector2i {.discardable.} =
   v.x = x
   return v
 
-proc setY*(v: Vector3i, y: int): Vector3i {.discardable.} =
+proc setY*(v: Vector2i, y: int): Vector2i {.discardable.} =
   v.y = y
   return v
 
-
-
+proc toString*(v: Vector2i): string =
+  return "Vector2i(" & $v.x & ", " & $v.y & ")"
