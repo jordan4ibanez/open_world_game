cbc0bb2844609b3d961ae1c374ec961f3f015a2e
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-08-09 19:56:00 -0400

    add func

--- src/noml/vector2i.nim
+++ src/noml/vector2i.nim
@@ -34,5 +34,17 @@ proc subtract*(v, other, output: Vector2i): Vector2i =
     v.y - other.y
   )
 
+proc add*(v, other: Vector2i): Vector2i =
+  result.set(
+    v.x + other.x,
+    v.y + other.y
+  )
+
+proc add*(v, other, output: Vector2i): Vector2i =
+  return output.set(
+    v.x + other.x,
+    v.y + other.y
+  )
+
 proc toString*(v: Vector2i): string =
   return "Vector2i(" & $v.x & ", " & $v.y & ")"
