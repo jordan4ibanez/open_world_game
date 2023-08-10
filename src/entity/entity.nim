7b19d251b474fcaf6a2e3028cddd62bd5f496d2b
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-08-10 19:39:28 -0400

    Add some things

--- src/entity/entity.nim
+++ src/entity/entity.nim
@@ -1,13 +1,32 @@
 import ../noml/vector2i
+import std/tables
+import uuids
 
+#[
+  Holds the blueprints for entitis.
+]#
 type
+
   Entity = ref object of RootObj
     position: Vector2i
   
+  Item = ref object of Entity
+    name: string
+  
   LivingEntity = ref object of Entity
-    health: int = 20
+    health: int
 
   Player* = ref object of LivingEntity
 
   Zombie* = ref object of LivingEntity
+
+
+var items = initTable[string, Item]()
+
+proc addItem*(name: string, position: Vector2i) =
+  let id: string = $genUUID()
+  items[id] = Item(name: name, position: position)
+
+proc addItem*(name: string, x,y: int) =
+  addItem(name, newVector2i(x, y))
 
