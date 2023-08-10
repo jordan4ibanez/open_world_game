bc16afe5896afd7b99e12d7854e0082fed3d3ad6
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-08-10 05:08:11 -0400

    Some basic nonsense

---
+++ src/entity_prototypes/entity.nim
@@ -0,0 +1,12 @@
+import ../noml/vector2i
+
+type
+  Entity = ref object of RootObj
+    position: Vector2i
+  
+  LivingEntity = ref object of Entity
+    health: int = 20
+
+  Player* = ref object of LivingEntity
+
+  Zombie* = ref object of LivingEntity
