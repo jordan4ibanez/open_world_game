c0f84047e3e8da0f451407871a8774095add1e69
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-08-11 17:15:32 -0400

    Neaten this up

--- src/open_world_game.nim
+++ src/open_world_game.nim
@@ -3,37 +3,10 @@ import entity/entity
 import std/strutils
 import utils/print
 import raylib
+import raylib_helpers/shapes
 
 var running: bool = true
 
-# proc parseInput(input: string): seq[string] =
-#   return input.split
-
-# proc interpretCommand(input: seq[string]) =
-#   case input[0].toLower:
-#   of "forward":
-#     # do things
-#     echo "hi"
-#   of "backward":
-#     # do things
-#     echo "hi"
-#   of "left":
-#     # do things
-#     echo "hi"
-#   of "right":
-#     # do things
-#     echo "hi"
-#   of "":
-#     echo "You must type something!"
-#   of "quit", "exit", "leave", "bye", "seeya":
-#     running = false
-#   else:
-#     println("Don't know how to " & input)
-
-# while (running):
-  # print "> "
-  # let input = parseInput readLine stdin
-  # interpretCommand input
 
 #[
   Game entry point, init, loop, and exit.
@@ -42,14 +15,20 @@ proc initialize =
   initWindow(800, 600, "Hello")
   setTargetFPS(60)
 
+
 proc mainLoop =
   # Update procedure.
+
+  var person = RectangleRef(x: 100, y: 100, width: 220, height: 220)
+
 
   # Draw procedure.
   beginDrawing()
   clearBackground(Black)
   drawText("hi", 0,0,32, RayWhite)
+  drawRectangle(person, Red)
   endDrawing()
+
 
 proc cleanUp =
   closeWindow()
@@ -58,11 +37,11 @@ proc cleanUp =
 
 #[
   The above functional processes implemented in a nim-friendly manor.
+  Condensed into this mini blob.
 ]#
 proc main =
   initialize()
   while not windowShouldClose():
     mainLoop()
   cleanUp()
-
 main()
