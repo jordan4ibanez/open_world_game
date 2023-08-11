3cd0be9122ab1032546faaf2746030acf55f7c30
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-08-10 20:28:52 -0400

    Now bootstrap it to be generic

--- src/entity/entity.nim
+++ src/entity/entity.nim
@@ -8,6 +8,7 @@ import uuids
 type
 
   Entity = ref object of RootObj
+    id: string
     position: Vector2i
   
   Item = ref object of Entity
@@ -20,29 +21,66 @@ type
 
   Zombie* = ref object of LivingEntity
 
+#[
+  Basic entity things
+]#
 proc getPosition*(entity: Entity): Vector2i =
   return entity.position
 
-var items = initTable[string, Item]()
+proc getID*(entity: Entity): string =
+  return entity.id
 
-proc addItem*(name: string, position: Vector2i) =
+proc getName*(item: Item): string =
+  return item.name
+
+proc getHealth*(livingEntity: LivingEntity): int =
+  return livingEntity.health
+
+#[
+  Generic container implementation so I don't accidentally break things.
+  Which has happened plenty times before.
+  This is a wrapper around a table.
+]#
+type
+  Container[T] = ref object of RootObj
+    values: Table[string, T]
+
+proc add*(container: Container, name: string, position: Vector2i) =
   let id: string = $genUUID()
-  items[id] = Item(name: name, position: position)
+  container.values[id] = Item(name: name, position: position)
 
-proc addItem*(name: string, x,y: int) =
-  addItem name, newVector2i(x, y)
+proc add*(container: Container, name: string, x,y: int) =
+  add container, name, newVector2i(x, y)
 
-proc hasItem*(id: string): bool =
-  return items.hasKey id
+proc has*(container: Container, id: string): bool =
+  return container.values.hasKey id
 
-proc getItem*(id: string): Item =
-  if not hasItem id:
-    raise newException(KeyError, "Could not find item " & id)
-  return items[id]
+proc get*(container: Container, id: string): Item =
+  if not has id:
+    raise newException(KeyError, "Could not find " & id)
+  return container.values[id]
 
-proc itemsInRadius*(position: Vector2i, radius: float): seq[Item] =
-  for item in items.values:
+proc remove*(container: Container, id: string) =
+  if not has id:
+    raise newException(KeyError, "could not find" & id)
+  container.values.del id
+
+proc getInRadius*(container: Container, position: Vector2i, radius: float): seq[Item] =
+  for item in container.values.values:
     if position.distance(item.getPosition) <= radius:
       result.add item
 
+proc getAll*(container: Container): Table =
+  return container.values.values
+
+#[
+  Entity containers.
+]#
+var Items* = Container[Item]()
+var Zombies* = Container[Zombie]()
+
+#[
+  The only player in the game.
+]#
+var SinglePlayer* = Player()
 
