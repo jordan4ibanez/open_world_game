e5cf92a4ffa2b4ae95257645547be24c05a46cc7
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-08-10 21:23:20 -0400

    BREAD IS BACK ON THE MENU

--- src/open_world_game.nim
+++ src/open_world_game.nim
@@ -1,3 +1,11 @@
 import noml/vector2i
 import entity/entity
 
+
+Items.add("bread", 0, 0)
+
+for item in Items.getInRadius(newVector2i(0,0), 5):
+  let id = item.getID
+
+for item in Items.getAll:
+  echo item[]
