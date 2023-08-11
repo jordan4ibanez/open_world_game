afcf3f054a0d97f8347add70f4c70963c4dfbc7c
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-08-11 01:21:45 -0400

    Time to do  the thing

--- src/open_world_game.nim
+++ src/open_world_game.nim
@@ -1,35 +1,35 @@
-import noml/vector2i
+import noml/vector2f
 import entity/entity
 import std/strutils
 import utils/print
 
 var running: bool = true
 
-proc parseInput(input: string): seq[string] =
-  return input.split
+# proc parseInput(input: string): seq[string] =
+#   return input.split
 
-proc interpretCommand(input: seq[string]) =
-  case input[0].toLower:
-  of "forward":
-    # do things
-    echo "hi"
-  of "backward":
-    # do things
-    echo "hi"
-  of "left":
-    # do things
-    echo "hi"
-  of "right":
-    # do things
-    echo "hi"
-  of "":
-    echo "You must type something!"
-  of "quit", "exit", "leave", "bye", "seeya":
-    running = false
-  else:
-    println("Don't know how to " & input)
+# proc interpretCommand(input: seq[string]) =
+#   case input[0].toLower:
+#   of "forward":
+#     # do things
+#     echo "hi"
+#   of "backward":
+#     # do things
+#     echo "hi"
+#   of "left":
+#     # do things
+#     echo "hi"
+#   of "right":
+#     # do things
+#     echo "hi"
+#   of "":
+#     echo "You must type something!"
+#   of "quit", "exit", "leave", "bye", "seeya":
+#     running = false
+#   else:
+#     println("Don't know how to " & input)
 
-while (running):
-  print "> "
-  let input = parseInput readLine stdin
-  interpretCommand input
+# while (running):
+  # print "> "
+  # let input = parseInput readLine stdin
+  # interpretCommand input
