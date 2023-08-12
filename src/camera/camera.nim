78ca8dfe724bfd02f6c7e01c3418eb2504d511f9
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-08-11 21:37:57 -0400

    Create camera.nim

---
+++ src/camera/camera.nim
@@ -0,0 +1,15 @@
+import raylib
+import ../noml/vector2f
+import ../entity/entity
+
+
+var Cam* = Camera2D(
+  target: newVector2f(0,0),
+  offset: newVector2f(0,0),
+  rotation: 0,
+  zoom: 1
+)
+
+proc update*(cam: var Camera2D) =
+  cam.target = SinglePlayer.getPosition()
+  cam.offset = newVector2f((float)(getRenderWidth()) / 2.0,(float)(getRenderHeight()) / 2.0)
