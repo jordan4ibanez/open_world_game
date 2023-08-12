a46da33db21aa28e7e2cc1494fc7530535e7abaf
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-08-11 21:38:16 -0400

    Now make this thing nicer

--- src/open_world_game.nim
+++ src/open_world_game.nim
@@ -1,6 +1,8 @@
 import noml/vector2f
+
 import std/math
 import std/strutils
+import camera/camera
 import entity/entity
 import utils/print
 import raylib
@@ -13,21 +15,30 @@ var running: bool = true
   Game entry point, init, loop, and exit.
 ]#
 proc initialize =
+  setConfigFlags(flags(WindowResizable))
   initWindow(800, 600, "Hello")
   setTargetFPS(60)
 
 
+var yaw = 0.0
 
 proc mainLoop =
   # Update procedure.
 
+  yaw += 10
 
   # Draw procedure.
   beginDrawing()
+  Cam.update()
+  beginMode2D(Cam)
   clearBackground(Black)
   drawText("hi", 0,0,32, RayWhite)
 
+  let rectangle = newRectangle(SinglePlayer.getPosition(),100,200)
+  
+  drawRectangle(rectangle, rectangle.getCenter(), yaw, Red)
 
+  endMode2D()
   endDrawing()
 
 
