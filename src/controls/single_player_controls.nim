041aea7cd50553cbbb468c2210ef3fa4a806579c
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-08-13 06:23:28 -0400

    Add some basic controls

--- src/controls/single_player_controls.nim
+++ src/controls/single_player_controls.nim
@@ -2,6 +2,7 @@ import raylib
 import ../noml/vector2f
 import ../entity/entity
 import mouse
+import keyboard
 import std/math
 
 #[
@@ -10,8 +11,25 @@ import std/math
 
 proc controlPlayer* =
 
-  let directionVector = SinglePlayer.getPosition() - Mouse.getWorldPosition()
-
-  let yaw = radToDeg arctan2(directionVector.getY(), directionVector.getX())
+  let yaw = SinglePlayer.getPosition().yaw(Mouse.getWorldPosition())
 
   SinglePlayer.setYaw(yaw)
+
+  var pos = SinglePlayer.getPosition()
+  
+  var v = newVector2f(0,0)
+
+  if Keyboard.keyDown("d"):
+    pos = pos + v.set(5,0)
+  if Keyboard.keyDown("a"):
+    pos = pos + v.set(-5,0)
+  if Keyboard.keyDown("w"):
+    pos = pos + v.set(0,-5)
+  if Keyboard.keyDown("s"):
+    pos = pos + v.set(0,5)
+
+  
+  SinglePlayer.setPosition(pos)
+
+
+  
