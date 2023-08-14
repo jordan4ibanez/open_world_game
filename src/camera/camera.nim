b8b412a4f1fb8a907c50a4598753f011a9725a38
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-08-13 20:24:33 -0400

    Cam is zoomable NOICE

--- src/camera/camera.nim
+++ src/camera/camera.nim
@@ -2,7 +2,6 @@ import raylib
 import ../noml/vector2f
 import ../entity/entity
 
-
 var Cam* = Camera2D(
   target: newVector2f(0,0),
   offset: newVector2f(0,0),
@@ -13,10 +12,12 @@ var Cam* = Camera2D(
 proc update*(cam: var Camera2D) =
   cam.target = SinglePlayer.getPosition()
   cam.offset = newVector2f((float)(getRenderWidth()) / 2.0,(float)(getRenderHeight()) / 2.0)
-  cam.zoom = 0.5
-  
+
 proc getPosition*(cam: Camera2D): Vector2f =
   return cam.target - cam.offset
 
 proc getZoom*(cam: Camera2D): float =
   cam.zoom
+
+proc setZoom*(cam: var Camera2D, newZoom: float) =
+  cam.zoom = newZoom
