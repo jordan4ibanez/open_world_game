003bc32c72522d16fbd9c517bfbb89670910fdb4
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-08-16 16:57:18 -0400

    This is a mess

--- src/open_world_game.nim
+++ src/open_world_game.nim
@@ -36,6 +36,13 @@ proc initialize =
   maximizeWindow()
 
 var shadowRotation = 0.0
+var armSize = 75.0
+var halfArmSize = armSize / 2.0
+var armScale = 1.0
+var minArmScale = 1.0
+var maxArmScale = 5.0
+var animationSize = 0.05
+var up = true
 
 proc mainLoop =
   # Update procedure.
@@ -47,6 +54,17 @@ proc mainLoop =
   shadowRotation += 0.1
   if shadowRotation > PI * 2:
     shadowRotation -= PI * 2
+  
+  if up:
+    armScale += animationSize
+    if armScale >= maxArmScale:
+      armScale = maxArmScale
+      up = false
+  else:
+    armScale -= animationSize
+    if armScale <= minArmScale:
+      armScale = minArmScale
+      up = true
 
 
 
@@ -59,26 +77,43 @@ proc mainLoop =
 
   drawText("hi", 0,0,32, RayWhite)
 
-  var rectangle = newRectangle(SinglePlayer.getPosition(), 100, 150)
+  var rectangle = newRectangle(SinglePlayer.getPosition(), 75, 150)
 
   # Render the player.
   drawRectangle(rectangle, rectangle.getCenter(), SinglePlayer.getYaw(), Red)
 
+  block:
+    rectangle.setSize(armSize * armScale,armSize)
+    # Left arm.
+    # Move out.
+    rectangle.setPosition newVector2f((degToRad(SinglePlayer.getYaw())).direction(150 - (halfArmSize))) + SinglePlayer.getPosition()
+    # Move forwards.
+    rectangle.setPosition newVector2f(rectangle.getPosition()) + (degToRad(SinglePlayer.getYaw()) + (PI / 2.0)).direction(((armSize * armScale) / 2.0) - halfArmSize)
+    drawRectangle(rectangle, rectangle.getCenter(), SinglePlayer.getYaw(), Blue)
+
+    # Right arm.
+    # Move out.
+    rectangle.setPosition newVector2f((degToRad(SinglePlayer.getYaw() + 180.0)).direction(150 - (halfArmSize))) + SinglePlayer.getPosition()
+    # Move forwards.
+    rectangle.setSize(armSize * (abs(maxArmScale - armScale) + 1.0),armSize)
+    rectangle.setPosition newVector2f(rectangle.getPosition()) + (degToRad(SinglePlayer.getYaw()) + (PI / 2.0)).direction(((armSize * (abs(armScale - maxArmScale) + 1.0)) / 2.0) - halfArmSize)
+    drawRectangle(rectangle, rectangle.getCenter(), SinglePlayer.getYaw(), Green)
+
   # Render the zombies.
-  for zombie in Zombies.getAll():
-    rectangle.setPosition(zombie.getPosition())
-    zombie.setYaw(zombie.getPosition().yaw(Mouse.getWorldPosition()))
-    drawRectangle(rectangle, rectangle.getCenter(), zombie.getYaw(), DarkGreen)
+  # for zombie in Zombies.getAll():
+  #   rectangle.setPosition(zombie.getPosition())
+  #   zombie.setYaw(zombie.getPosition().yaw(Mouse.getWorldPosition()))
+  #   drawRectangle(rectangle, rectangle.getCenter(), zombie.getYaw(), DarkGreen)
 
-  # Render the zombie's UUID.
-  Text.setSize(30)
-  Text.setColor(Green)
-  Text.setShadowColor(Blue)
-  Text.setShadowOffset((shadowRotation * -1.0).direction 2)
+  # # Render the zombie's UUID.
+  # Text.setSize(30)
+  # Text.setColor(Green)
+  # Text.setShadowColor(Blue)
+  # Text.setShadowOffset((shadowRotation * -1.0).direction 2)
 
-  for zombie in Zombies.getAll():
-    # echo zombie.getID
-    textDrawCentered(zombie.getID, zombie.getPosition + newVector2f(0,-100))
+  # for zombie in Zombies.getAll():
+  #   # echo zombie.getID
+  #   textDrawCentered(zombie.getID, zombie.getPosition + newVector2f(0,-100))
 
   drawCircle(Mouse.getWorldPosition(), 4, White)
 
